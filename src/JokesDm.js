84d7475f7611e8b56ac8d84aa2bf69b4f66613d4
Eduardo Cruz Zanchez <eduardo.cruz.zanchez@bbva.com>
2023-03-31 11:55:01 -0600

    complete response from api

--- src/JokesDm.js
+++ src/JokesDm.js
@@ -1,6 +1,7 @@
 import { LitElement, html, } from 'lit-element';
 import { getComponentSharedStyles } from '@bbva-web-components/bbva-core-lit-helpers';
 import styles from './JokesDm-styles.js';
+import '@bbva-web-components/bbva-core-generic-dp/bbva-core-generic-dp'
 /**
 ![LitElement component](https://img.shields.io/badge/litElement-component-blue.svg)
 
@@ -41,11 +42,82 @@ export class JokesDm extends LitElement {
     ];
   }
 
+  normalizeResponse(resp) {
+    //opcion si trae 2 respuestas
+    if(resp.jokes){
+      return resp.jokes.map((r) => {
+        const {setup, delivery, ...rest} = r;
+        return{
+          joke: `${setup} \n ${delivery}`,   
+          ...rest,
+        }
+      })
+    }
+    //Una respuesta
+    const {error, ...rest} = resp;
+    const result = {
+      error,
+      amount:1,
+      jokes: [
+        {
+       joke: `${setup} \n ${delivery}`,          
+      ...rest,
+          }
+        ],
+    };
+    console.log(result);
+    return result;
+  }
+
+  firstUpdated() {
+    const athleapRecipesDP = this.shadowRoot.querySelector(
+      '#jokes-dm'
+    );
+    // 2. Lanzar un evento, para avisar que el request iniciará
+    this.dispatchEvent(
+      new CustomEvent('jokes-dm-request-start', {
+        detail: true,
+        bubbles: true,
+        composed: true,
+      })
+    );
+    // 3. Ejecutar el DP
+    athleapRecipesDP
+      .generateRequest()
+      .then(({ response }) => {
+        console.log(response)
+        // 4-A. Normalizar la respuesta
+        const normalizedResponse = this.normalizeResponse(response);
+        // 5-A. Reaccionar a la respuesta exitosa
+        /*this.dispatchEvent(
+          new CustomEvent('jokes-dm-success', {
+            bubbles: true,
+            composed: true,
+            detail: normalizedResponse,
+          })
+        );*/
+      })
+      // 5-B. Reaccionar a la respuesta fallida
+      .catch((err) => {
+        this.dispatchEvent(
+          new CustomEvent('jokes-dm-error', {
+            bubbles: true,
+            composed: true,
+            detail: err,
+          })
+        );
+      });
+  }
+
   // Define a template
   render() {
     return html`
-      <slot></slot>
-      <p>Welcome to ${this.name}</p>
+    <bbva-core-generic-dp
+        id="jokes-dm"
+        method="GET"
+        host="https://v2.jokeapi.dev"
+        path="/joke/Any?type=twopart&amount=2"
+      ></bbva-core-generic-dp>
     `;
   }
 }
